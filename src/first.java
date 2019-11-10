8b69f8a48b6ee13402e3f299a5d3ae73ef6340e8
12hleb <56984619+12hleb@users.noreply.github.com>
2019-11-10 11:40:57 -0600

    Update first.java

--- src/first.java
+++ src/first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("first");
+                System.out.println("2th");
+                System.out.println("third");
+
     }
 }
